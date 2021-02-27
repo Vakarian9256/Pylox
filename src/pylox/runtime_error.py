0aa08d1852d9fb32f1328dfe6e4b4319d14717c0
ronsh909 <ronsh909@gmail.com>
2021-02-27 15:55:08 +0200

    r

--- src/pylox/runtime_error.py
+++
@@ -1,7 +0,0 @@
-import sys
-from token import Token
-
-class LoxRunTimeError(RuntimeError):
-    def __init__(self, token: Token, message: str):
-        super().__init__(message)
-        self.token = token
