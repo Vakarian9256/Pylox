fa8570c05d0f99ac96d0433d1acce9b6b7022d5f
ronsh909 <ronsh909@gmail.com>
2021-03-21 01:34:53 +0200

    Fixed the value that is returned from read()
    Fixed the _typify function to correctly return strings.
    Added support to read nil.
    Fixed the _typify function to correctly receive an empty line - a line which holds only a newline character.
    Fixed typo in convert_to_number.

--- src/pyLox/input_util.py
+++ src/pyLox/input_util.py
@@ -2,17 +2,19 @@
 The module houses 3 utility functions that are used in the Lox Native Library input function - read.
 '''
 def _typify(string: str):
-    if string[0] == '"':
-        return string
-    value = conver_to_number(string)
-    if value.is_integer():
-        return int(value)
+    print(string)
+    if string == 'nil':
+        return None
+    value = convert_to_number(string)
+    if type(value) is float:
+        if value.is_integer():
+            return int(value)
     return value
 
 def is_digit(char: chr ) -> bool:
         return char >= '0' and char <= '9'
 
-def conver_to_number(string: str):
+def convert_to_number(string: str):
     index = 0
     while index < len(string) and is_digit(string[index]):
         index += 1
@@ -20,4 +22,6 @@ def conver_to_number(string: str):
         index += 1
         while index < len(string) and is_digit(string[index]):
             index += 1
-    return float(string)
+    if index == len(string) and string != '':
+        return float(string)
+    return string
